a9bed8484af5326054a90552158b8e9d05a03641
Tristan Eastman <teastman85@gmail.com>
2020-08-22 13:39:09 -0400

    Add button.

--- assets/chuckScripts.js
+++ assets/chuckScripts.js
@@ -1,29 +1,31 @@
-$("#chuck-button").on("click", function () {
 
-    // Storing our giphy API URL for a random cat image
-    var queryURL = "http://api.icndb.com/jokes/random/3";
+    // Event listener for our cat-button
+    $("#chuck-button").on("click", function() {
 
-    // Perfoming an AJAX GET request to our queryURL
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    })
-
+        // Storing our giphy API URL for a random cat image
+        var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=cats";
+  
+        // Perfoming an AJAX GET request to our queryURL
+        $.ajax({
+          url: queryURL,
+          method: "GET"
+        })
+  
         // After the data from the AJAX request comes back
-        .then(function (response) {
+          .then(function(response) {
             console.log(response);
-
-            // // Saving the image_original_url property
-            // var imageUrl = response.data.image_original_url;
-
-            // // Creating and storing an image tag
-            // var catImage = $("<img>");
-
-            // // Setting the catImage src attribute to imageUrl
-            // catImage.attr("src", imageUrl);
-            // catImage.attr("alt", "cat image");
-
-            // // Prepending the catImage to the images div
-            // $("#images").prepend(catImage);
-        });
-});
+  
+          // Saving the image_original_url property
+            var imageUrl = response.data.image_original_url;
+  
+            // Creating and storing an image tag
+            var catImage = $("<img>");
+  
+            // Setting the catImage src attribute to imageUrl
+            catImage.attr("src", imageUrl);
+            catImage.attr("alt", "cat image");
+  
+            // Prepending the catImage to the images div
+            $("#images").prepend(catImage);
+          });
+      });
