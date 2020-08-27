85eae5d6fefa2463c41a3d9a585d45a0c193bbda
Tristan Eastman <teastman85@gmail.com>
2020-08-27 08:12:54 -0400

    Working featured carousel with fade.

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,41 @@
 $(document).ready(function(){
     $('.carousel').slick({
+        dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    autoplay: true
       
     });
+   
   });
+
+  // Start Unused code to be deleted before presentation
+//   $('.fade').slick({
+//     dots: true,
+//     infinite: true,
+//     speed: 500,
+//     fade: true,
+//     cssEase: 'linear'
+//   });
+  
+//   $('.slider-for').slick({
+//     slidesToShow: 1,
+//     slidesToScroll: 1,
+//     arrows: false,
+//     fade: true,
+//     // asNavFor: '.slider-nav'
+//   });
+//   $('.slider-nav').slick({
+//     slidesToShow: 3,
+//     slidesToScroll: 1,
+//     // asNavFor: '.slider-for',
+//     dots: true,
+//     centerMode: true,
+//     focusOnSelect: true
+//   });
+  // End Unused code to be deleted before presentation
 
 var Calendar = tui.Calendar;
 var releaseTitle = "";
